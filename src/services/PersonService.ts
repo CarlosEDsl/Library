983f9288b275e2a3e346bc9778ab49b2855c1dda
CarlosEDsl <carloseduardoestds@gmail.com>
2024-08-12 13:31:49 -0300

    feat: personService finished

--- src/services/PersonService.ts
+++ src/services/PersonService.ts
@@ -13,6 +13,33 @@ export class PersonService {
         return await this.personRepository.insertPerson(person);
     }
 
+    async updatePerson(person:Person) {
+        if((await this.personRepository.findPersonById(person.id)).email != person.email)
+            this.emailVerifier(person.email);
+
+        return await this.personRepository.updatePerson(person);
+    }
+
+    async deletePerson(person:Person) {
+        const deletePersonId = await this.personRepository.findPersonById(person.id);
+        const deletePersonEmail =  await this.personRepository.findPersonByEmail(person.email);
+
+        if(deletePersonId != deletePersonEmail) {
+            throw new Error("Email and ID don't match");
+        }
+
+        return await this.personRepository.deletePerson(person.id);
+    }
+    
+    async findPerson(id:number) {
+        const person = await this.personRepository.findPersonById(id);
+        return person;
+    }
+
+    async getAllPerson() {
+        return await this.personRepository.findAllPersons();
+    }
+
     async emailVerifier(email:string) {
         if(this.personRepository.findPersonByEmail(email) != null) {
             throw new Error("email already in use");
